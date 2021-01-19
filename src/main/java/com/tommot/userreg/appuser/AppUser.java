cbaa6a3e2b3d377919999c2dd06511130d9bd7b1
Tosin <{ID}+{username}@users.noreply.github.com>
2021-01-19 03:09:26 +0100

    implement Generator Closes issues #3

--- src/main/java/com/tommot/userreg/appuser/AppUser.java
+++ src/main/java/com/tommot/userreg/appuser/AppUser.java
@@ -1,13 +1,34 @@
 package com.tommot.userreg.appuser;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 public class AppUser implements UserDetails {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
@@ -16,7 +37,21 @@ public class AppUser implements UserDetails {
     private Boolean locked;
     private Boolean enabled;
 
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
+
+
+    public AppUser(String firstName, String lastName, String username,
+                   String password,  Boolean locked, Boolean enabled, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.locked = locked;
+        this.enabled = enabled;
+        this.appUserRole = appUserRole;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
