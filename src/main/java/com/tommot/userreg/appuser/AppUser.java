15c0cb159f515e9fe27c347d1c4eec7e872b0607
Tosin <{ID}+{username}@users.noreply.github.com>
2021-01-19 02:53:16 +0100

    Setup and initial commit

---
+++ src/main/java/com/tommot/userreg/appuser/AppUser.java
@@ -0,0 +1,50 @@
+package com.tommot.userreg.appuser;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AppUser implements UserDetails {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    private AppUserRole appUserRole;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/tommot/userreg/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.tommot.userreg.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
