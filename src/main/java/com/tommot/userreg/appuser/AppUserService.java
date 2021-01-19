effb19265c5c8665eaff02f7411516109ba7f006
Tosin <{ID}+{username}@users.noreply.github.com>
2021-01-19 03:51:17 +0100

    implement RegistrationController

--- src/main/java/com/tommot/userreg/appuser/AppUserService.java
+++ src/main/java/com/tommot/userreg/appuser/AppUserService.java
@@ -13,6 +13,8 @@ public class AppUserService implements UserDetailsService {
     private final AppUserRepository appUserRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE, email)));
+        return appUserRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE, email)));
     }
 }
+
---
+++ src/main/java/com/tommot/userreg/registration/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.tommot.userreg.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        // For now
+        return "works";
+    }
+}
