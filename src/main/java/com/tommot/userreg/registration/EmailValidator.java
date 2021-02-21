b050e2d4503d2f9bad66c8be716850c526a938a5
Tosin <tosintubi@yahoo.co.uk>
2021-02-21 01:24:38 +0100

    add @Service annotation tag to EmailValidator

--- src/main/java/com/tommot/userreg/registration/EmailValidator.java
+++ src/main/java/com/tommot/userreg/registration/EmailValidator.java
@@ -7,14 +7,7 @@ import java.util.function.Predicate;
 @Service
 public class EmailValidator  implements Predicate<String> {
 
-    /**
-     * Evaluates this predicate on the given argument.
-     *
-     * @param s the input argument
-     * @return {@code true} if the input argument matches the predicate,
-     * otherwise {@code false}
-     */
-    
+   
     @Override
     public boolean test(String s) {
         // TODO: Regex to validate email. - Lets assume all is valid for now
