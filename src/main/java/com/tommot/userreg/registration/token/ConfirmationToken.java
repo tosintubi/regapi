e482f4e458f487f2a3636b38d1577dd23c8d0ce7
Tosin <tosintubi@users.noreply.github.com>
2021-02-21 02:22:58 +0100

    ConfirmationToken model

--- src/main/java/com/tommot/userreg/registration/token/ConfirmationToken.java
+++ src/main/java/com/tommot/userreg/registration/token/ConfirmationToken.java
@@ -4,23 +4,41 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class ConfirmationToken {
+
+    @Id
+    @SequenceGenerator(
+            name = "token_sequence",
+            sequenceName = "token_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "token_sequence"
+    )
     private Long id;
+
+    @Column(nullable = false)
     private String token;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
-    private LocalDateTime expiredAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
     private LocalDateTime confirmedAt;
 
     public ConfirmationToken(String token, LocalDateTime createdAt,
                              LocalDateTime expiredAt, LocalDateTime confirmedAt) {
         this.token = token;
         this.createdAt = createdAt;
-        this.expiredAt = expiredAt;
+        this.expiresAt = expiredAt;
         this.confirmedAt = confirmedAt;
     }
 }
